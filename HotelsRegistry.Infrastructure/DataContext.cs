9d31d9d6b0ce1b988d1a73dfe73c12e8a3a082d4
danrug92 <dnny_rm@hotmail.it>
2025-02-20 12:10:18 +0100

    Set explicit foreign keys for RoomTypeBaseId and RoomTypeRelatedId

--- HotelsRegistry.Infrastructure/DataContext.cs
+++ HotelsRegistry.Infrastructure/DataContext.cs
@@ -16,5 +16,19 @@ namespace HotelsRegistry.Infrastructure
         public virtual DbSet<RoomHierarchy> RoomHierarchys { get; set; }
         public virtual DbSet<Room> Rooms { get; set; }
         public virtual DbSet<Pricing> Pricings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RoomHierarchy>()
+                .HasOne(rh => rh.RoomTypeBase)
+                .WithMany() // Se non c'è una collection in RoomType, altrimenti specifica il nome della proprietà di navigazione
+                .HasForeignKey("RoomTypeBaseId") // Assicurati che la FK esista nel modello
+                .OnDelete(DeleteBehavior.Restrict); // Evita eliminazioni a cascata indesiderate
+            modelBuilder.Entity<RoomHierarchy>()
+                .HasOne(rh => rh.RoomTypeRelated)
+                .WithMany() 
+                .HasForeignKey("RoomTypeRelatedId") 
+                .OnDelete(DeleteBehavior.Restrict); 
+        }
     }
 }
