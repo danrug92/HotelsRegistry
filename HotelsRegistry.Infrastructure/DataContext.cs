a9319597808c7b02ef7d4280aa4b6c6458e9a71b
danrug92 <dnny_rm@hotmail.it>
2025-02-20 11:50:22 +0100

    add data context and set connection string to db on program from infrastructure layer

---
+++ HotelsRegistry.Infrastructure/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using HotelsRegistry.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelsRegistry.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Accommodation> Accommodations { get; set; }
+        public virtual DbSet<RoomType> RoomTypes { get; set; }
+        public virtual DbSet<RoomHierarchy> RoomHierarchys { get; set; }
+        public virtual DbSet<Room> Rooms { get; set; }
+        public virtual DbSet<Pricing> Pricings { get; set; }
+    }
+}
---
+++ HotelsRegistry.Infrastructure/InfrastructureConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsRegistry.Infrastructure
+{
+    public static class InfrastructureConfig
+    {
+        public static void AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
