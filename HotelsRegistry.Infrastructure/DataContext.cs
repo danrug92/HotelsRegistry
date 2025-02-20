f742df57099a60d19cf37fd9a059a108af6eb95e
danrug92 <dnny_rm@hotmail.it>
2025-02-20 12:36:39 +0100

    Add development document

--- HotelsRegistry.Infrastructure/DataContext.cs
+++ HotelsRegistry.Infrastructure/DataContext.cs
@@ -21,9 +21,9 @@ namespace HotelsRegistry.Infrastructure
         {
             modelBuilder.Entity<RoomHierarchy>()
                 .HasOne(rh => rh.RoomTypeBase)
-                .WithMany() // Se non c'è una collection in RoomType, altrimenti specifica il nome della proprietà di navigazione
-                .HasForeignKey("RoomTypeBaseId") // Assicurati che la FK esista nel modello
-                .OnDelete(DeleteBehavior.Restrict); // Evita eliminazioni a cascata indesiderate
+                .WithMany() 
+                .HasForeignKey("RoomTypeBaseId") 
+                .OnDelete(DeleteBehavior.Restrict);
             modelBuilder.Entity<RoomHierarchy>()
                 .HasOne(rh => rh.RoomTypeRelated)
                 .WithMany() 
